f4dfbe884070d1f5b9cdf254702d3c631c1ce3ca
Eshaan0112 <eminocha@purdue.edu>
2023-04-06 12:36:59 -0400

    App

---
+++ homePage.js
@@ -0,0 +1,7 @@
+function runBot(){
+    var newDiv = document.createElement('div');
+    var textNode = document.createTextNode('Test App');
+    newDiv.appendChild(textNode);
+    document.body.appendChild(newDiv);
+
+}
